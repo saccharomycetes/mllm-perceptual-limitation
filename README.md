12aa487f3a90d367b123635a88729a964cd2139a
142857zjr <jzhang37@usc.edu>
2024-02-13 18:56:10 +0000

    my ;

--- README.md
+++ README.md
@@ -3,4 +3,41 @@ Code and data for paper 'Exploring Perceptual Limitation of Multimodal Large Lan
 
 [Jiarui Zhang](https://saccharomycetes.github.io/), [Jinyi Hu](https://jameshujy.github.io/), [Mahyar Khayatkhoei](https://mahyarkoy.github.io/),  [Filip Ilievski](https://www.ilievski.info/), [Maosong Sun](https://www.cs.tsinghua.edu.cn/csen/info/1180/4033.htm)
 
+[[paper]](https://arxiv.org/pdf/2402.07384.pdf)
 
+# Datasets Creation
+
+The following command will help you create all the images used for the experiments.(BLIP-2 and Instruct-BLIP use the same dataset due to the same resolusion.)
+```
+bash create.sh
+```
+
+# Running experiment
+
+Run the 5 models on the dataset:
+```
+bash run.sh
+```
+
+# Evaluate and draw the plots
+Then we can use the code in exp_plots.ipynb to evaluate the performance and draw the plots.
+
+
+# Cite 
+
+If you find our work to be useful in your research, please consider citing the following paper:
+
+```
+@misc{zhang2024exploring,
+      title={Exploring Perceptual Limitation of Multimodal Large Language Models}, 
+      author={Jiarui Zhang and Jinyi Hu and Mahyar Khayatkhoei and Filip Ilievski and Maosong Sun},
+      year={2024},
+      eprint={2402.07384},
+      archivePrefix={arXiv},
+      primaryClass={cs.CV}
+}
+```
+
+## Contact
+
+-   `jzhang37@usc.edu`
--- run.sh
+++ run.sh
@@ -1,71 +1,71 @@
-# bash run.sh fuyu-8b ./images/fuyu-8b/quality_3
-# bash run.sh fuyu-8b ./images/fuyu-8b/quality_5
-# bash run.sh fuyu-8b ./images/fuyu-8b/quality_7
-# bash run.sh fuyu-8b ./images/fuyu-8b/size_3
-# bash run.sh fuyu-8b ./images/fuyu-8b/size_5
-# bash run.sh fuyu-8b ./images/fuyu-8b/size_7
+bash run.sh fuyu-8b ./images/fuyu-8b/quality_3
+bash run.sh fuyu-8b ./images/fuyu-8b/quality_5
+bash run.sh fuyu-8b ./images/fuyu-8b/quality_7
+bash run.sh fuyu-8b ./images/fuyu-8b/size_3
+bash run.sh fuyu-8b ./images/fuyu-8b/size_5
+bash run.sh fuyu-8b ./images/fuyu-8b/size_7
 
-# bash run.sh blip2-flan-t5-xxl ./images/blip2-flan-t5-xxl/quality_3
-# bash run.sh blip2-flan-t5-xxl ./images/blip2-flan-t5-xxl/quality_5
-# bash run.sh blip2-flan-t5-xxl ./images/blip2-flan-t5-xxl/quality_7
-# bash run.sh blip2-flan-t5-xxl ./images/blip2-flan-t5-xxl/size_3
-# bash run.sh blip2-flan-t5-xxl ./images/blip2-flan-t5-xxl/size_5
-# bash run.sh blip2-flan-t5-xxl ./images/blip2-flan-t5-xxl/size_7
+bash run.sh blip2-flan-t5-xxl ./images/blip2-flan-t5-xxl/quality_3
+bash run.sh blip2-flan-t5-xxl ./images/blip2-flan-t5-xxl/quality_5
+bash run.sh blip2-flan-t5-xxl ./images/blip2-flan-t5-xxl/quality_7
+bash run.sh blip2-flan-t5-xxl ./images/blip2-flan-t5-xxl/size_3
+bash run.sh blip2-flan-t5-xxl ./images/blip2-flan-t5-xxl/size_5
+bash run.sh blip2-flan-t5-xxl ./images/blip2-flan-t5-xxl/size_7
 
-# bash run.sh instructblip-vicuna-13b ./images/blip2-flan-t5-xxl/quality_3
-# bash run.sh instructblip-vicuna-13b ./images/blip2-flan-t5-xxl/quality_5
-# bash run.sh instructblip-vicuna-13b ./images/blip2-flan-t5-xxl/quality_7
-# bash run.sh instructblip-vicuna-13b ./images/blip2-flan-t5-xxl/size_3
-# bash run.sh instructblip-vicuna-13b ./images/blip2-flan-t5-xxl/size_5
-# bash run.sh instructblip-vicuna-13b ./images/blip2-flan-t5-xxl/size_7
+bash run.sh instructblip-vicuna-13b ./images/blip2-flan-t5-xxl/quality_3
+bash run.sh instructblip-vicuna-13b ./images/blip2-flan-t5-xxl/quality_5
+bash run.sh instructblip-vicuna-13b ./images/blip2-flan-t5-xxl/quality_7
+bash run.sh instructblip-vicuna-13b ./images/blip2-flan-t5-xxl/size_3
+bash run.sh instructblip-vicuna-13b ./images/blip2-flan-t5-xxl/size_5
+bash run.sh instructblip-vicuna-13b ./images/blip2-flan-t5-xxl/size_7
 
-# bash run.sh llava-1.5-13b-hf ./images/llava-1.5-13b-hf/quality_3
-# bash run.sh llava-1.5-13b-hf ./images/llava-1.5-13b-hf/quality_5
-# bash run.sh llava-1.5-13b-hf ./images/llava-1.5-13b-hf/quality_7
-# bash run.sh llava-1.5-13b-hf ./images/llava-1.5-13b-hf/size_3
-# bash run.sh llava-1.5-13b-hf ./images/llava-1.5-13b-hf/size_5
-# bash run.sh llava-1.5-13b-hf ./images/llava-1.5-13b-hf/size_7
+bash run.sh llava-1.5-13b-hf ./images/llava-1.5-13b-hf/quality_3
+bash run.sh llava-1.5-13b-hf ./images/llava-1.5-13b-hf/quality_5
+bash run.sh llava-1.5-13b-hf ./images/llava-1.5-13b-hf/quality_7
+bash run.sh llava-1.5-13b-hf ./images/llava-1.5-13b-hf/size_3
+bash run.sh llava-1.5-13b-hf ./images/llava-1.5-13b-hf/size_5
+bash run.sh llava-1.5-13b-hf ./images/llava-1.5-13b-hf/size_7
 
-# bash run.sh Qwen-VL-Chat ./images/Qwen-VL-Chat/quality_3
-# bash run.sh Qwen-VL-Chat ./images/Qwen-VL-Chat/quality_5
-# bash run.sh Qwen-VL-Chat ./images/Qwen-VL-Chat/quality_7
-# bash run.sh Qwen-VL-Chat ./images/Qwen-VL-Chat/size_3
-# bash run.sh Qwen-VL-Chat ./images/Qwen-VL-Chat/size_5
-# bash run.sh Qwen-VL-Chat ./images/Qwen-VL-Chat/size_7
+bash run.sh Qwen-VL-Chat ./images/Qwen-VL-Chat/quality_3
+bash run.sh Qwen-VL-Chat ./images/Qwen-VL-Chat/quality_5
+bash run.sh Qwen-VL-Chat ./images/Qwen-VL-Chat/quality_7
+bash run.sh Qwen-VL-Chat ./images/Qwen-VL-Chat/size_3
+bash run.sh Qwen-VL-Chat ./images/Qwen-VL-Chat/size_5
+bash run.sh Qwen-VL-Chat ./images/Qwen-VL-Chat/size_7
 
-# bash run.sh fuyu-8b ./images/fuyu-8b/distract_8
-# bash run.sh blip2-flan-t5-xxl ./images/blip2-flan-t5-xxl/distract_8
-# bash run.sh instructblip-vicuna-13b ./images/blip2-flan-t5-xxl/distract_8
-# bash run.sh llava-1.5-13b-hf ./images/llava-1.5-13b-hf/distract_8
-# bash run.sh Qwen-VL-Chat ./images/Qwen-VL-Chat/distract_8
+bash run.sh fuyu-8b ./images/fuyu-8b/distract_8
+bash run.sh blip2-flan-t5-xxl ./images/blip2-flan-t5-xxl/distract_8
+bash run.sh instructblip-vicuna-13b ./images/blip2-flan-t5-xxl/distract_8
+bash run.sh llava-1.5-13b-hf ./images/llava-1.5-13b-hf/distract_8
+bash run.sh Qwen-VL-Chat ./images/Qwen-VL-Chat/distract_8
 
-# bash run.sh fuyu-8b ./images/fuyu-8b/distract_12
-# bash run.sh blip2-flan-t5-xxl ./images/blip2-flan-t5-xxl/distract_12
-# bash run.sh instructblip-vicuna-13b ./images/blip2-flan-t5-xxl/distract_12
-# bash run.sh llava-1.5-13b-hf ./images/llava-1.5-13b-hf/distract_12
-# bash run.sh Qwen-VL-Chat ./images/Qwen-VL-Chat/distract_12
+bash run.sh fuyu-8b ./images/fuyu-8b/distract_12
+bash run.sh blip2-flan-t5-xxl ./images/blip2-flan-t5-xxl/distract_12
+bash run.sh instructblip-vicuna-13b ./images/blip2-flan-t5-xxl/distract_12
+bash run.sh llava-1.5-13b-hf ./images/llava-1.5-13b-hf/distract_12
+bash run.sh Qwen-VL-Chat ./images/Qwen-VL-Chat/distract_12
 
-# bash run.sh fuyu-8b ./images/fuyu-8b/position_1
-# bash run.sh blip2-flan-t5-xxl ./images/blip2-flan-t5-xxl/position_1
-# bash run.sh instructblip-vicuna-13b ./images/blip2-flan-t5-xxl/position_1
-# bash run.sh llava-1.5-13b-hf ./images/llava-1.5-13b-hf/position_1
-# bash run.sh Qwen-VL-Chat ./images/Qwen-VL-Chat/position_1
-# bash run.sh Qwen-VL-Chat ./images/Qwen-VL-Chat/position_9
+bash run.sh fuyu-8b ./images/fuyu-8b/position_1
+bash run.sh blip2-flan-t5-xxl ./images/blip2-flan-t5-xxl/position_1
+bash run.sh instructblip-vicuna-13b ./images/blip2-flan-t5-xxl/position_1
+bash run.sh llava-1.5-13b-hf ./images/llava-1.5-13b-hf/position_1
+bash run.sh Qwen-VL-Chat ./images/Qwen-VL-Chat/position_1
+bash run.sh Qwen-VL-Chat ./images/Qwen-VL-Chat/position_9
 
-# bash run.sh fuyu-8b ./images/fuyu-8b/position_0
-# bash run.sh blip2-flan-t5-xxl ./images/blip2-flan-t5-xxl/position_0
-# bash run.sh instructblip-vicuna-13b ./images/blip2-flan-t5-xxl/position_0
-# bash run.sh llava-1.5-13b-hf ./images/llava-1.5-13b-hf/position_0
-# bash run.sh Qwen-VL-Chat ./images/Qwen-VL-Chat/position_0
+bash run.sh fuyu-8b ./images/fuyu-8b/position_0
+bash run.sh blip2-flan-t5-xxl ./images/blip2-flan-t5-xxl/position_0
+bash run.sh instructblip-vicuna-13b ./images/blip2-flan-t5-xxl/position_0
+bash run.sh llava-1.5-13b-hf ./images/llava-1.5-13b-hf/position_0
+bash run.sh Qwen-VL-Chat ./images/Qwen-VL-Chat/position_0
 
-# bash run.sh fuyu-8b ./images/fuyu-8b/hcut_6
-# bash run.sh blip2-flan-t5-xxl ./images/blip2-flan-t5-xxl/hcut_3
-# bash run.sh instructblip-vicuna-13b ./images/blip2-flan-t5-xxl/hcut_3
-# bash run.sh llava-1.5-13b-hf ./images/llava-1.5-13b-hf/hcut_3
-# bash run.sh Qwen-VL-Chat ./images/Qwen-VL-Chat/hcut_3
+bash run.sh fuyu-8b ./images/fuyu-8b/hcut_6
+bash run.sh blip2-flan-t5-xxl ./images/blip2-flan-t5-xxl/hcut_3
+bash run.sh instructblip-vicuna-13b ./images/blip2-flan-t5-xxl/hcut_3
+bash run.sh llava-1.5-13b-hf ./images/llava-1.5-13b-hf/hcut_3
+bash run.sh Qwen-VL-Chat ./images/Qwen-VL-Chat/hcut_3
 
-# bash run.sh fuyu-8b ./images/fuyu-8b/vcut_6
-# bash run.sh blip2-flan-t5-xxl ./images/blip2-flan-t5-xxl/vcut_3
-# bash run.sh instructblip-vicuna-13b ./images/blip2-flan-t5-xxl/vcut_3
-# bash run.sh llava-1.5-13b-hf ./images/llava-1.5-13b-hf/vcut_3
-# bash run.sh Qwen-VL-Chat ./images/Qwen-VL-Chat/vcut_3
+bash run.sh fuyu-8b ./images/fuyu-8b/vcut_6
+bash run.sh blip2-flan-t5-xxl ./images/blip2-flan-t5-xxl/vcut_3
+bash run.sh instructblip-vicuna-13b ./images/blip2-flan-t5-xxl/vcut_3
+bash run.sh llava-1.5-13b-hf ./images/llava-1.5-13b-hf/vcut_3
+bash run.sh Qwen-VL-Chat ./images/Qwen-VL-Chat/vcut_3
